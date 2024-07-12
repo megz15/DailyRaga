8461bb38af3a1412062119d1a7080b5da81de729
megz15 <meghraj.g16@gmail.com>
2024-07-13 02:53:32 +0530

    feat: get raga details

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,27 @@ Future<Map<String, String>> fetchRandomRaga() async {
   }
 }
 
+Future<Map<String, String>> fetchRagaDetails(String url) async {
+  Map<String, String> parsedData = {};
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    var ragaDetails =
+        parse(response.body).querySelector("tbody")!.querySelectorAll("tr");
+
+    for (var ragaDetail in ragaDetails) {
+      var ragaDetailList = ragaDetail.querySelectorAll("td");
+      parsedData[ragaDetailList[0].querySelector("a")!.text] =
+          ragaDetailList[1].text;
+    }
+
+    return parsedData;
+  } else {
+    throw Exception('Failed to load raga data');
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -67,7 +88,26 @@ class _MyHomePageState extends State<MyHomePage> {
                   if (snapshot.hasError) {
                     return Text('Error: ${snapshot.error}');
                   } else {
-                    return Text('Result: ${snapshot.data}');
+                    return Column(
+                      children: [
+                        Text(snapshot.data!["name"]!),
+                        FutureBuilder(
+                            future: fetchRagaDetails(snapshot.data!["url"]!),
+                            builder:
+                                (BuildContext ctx, AsyncSnapshot snapshot) {
+                              switch (snapshot.connectionState) {
+                                case ConnectionState.waiting:
+                                  return const Text('Loading....');
+                                default:
+                                  if (snapshot.hasError) {
+                                    return Text('Error: ${snapshot.error}');
+                                  } else {
+                                    return Text(snapshot.data.toString());
+                                  }
+                              }
+                            })
+                      ],
+                    );
                   }
               }
             }),
