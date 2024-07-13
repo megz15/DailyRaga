647d393d6c20fd01c50454402861d1b9dbbe6fd8
megz15 <meghraj.g16@gmail.com>
2024-07-13 14:25:02 +0530

    feat: raga paragraphs

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
 
 Future<Map<String, String>> fetchRandomRaga() async {
+  // return {"name": "Bhoopali", "url": "/wordpress/raag-bhoopali/"};
+
   final response =
       await http.get(Uri.parse("https://tanarang.com/raag-index/"));
 
@@ -19,24 +21,51 @@ Future<Map<String, String>> fetchRandomRaga() async {
   }
 }
 
-Future<List<Map<String, String>>> fetchRagaDetails(String url) async {
-  List<Map<String, String>> parsedData = [];
+Future<(List<Map<String, String>>, List<String>)> fetchRagaDetails(
+    String url) async {
+  // return [
+  //   {
+  //     "param": "Swar",
+  //     "val": "Gandhar and Nishad Komal. Rest all Shuddha Swaras."
+  //   },
+  //   {"param": "Jati", "val": "Sampurna - Sampurna"},
+  //   {"param": "Thaat", "val": "Kafi"},
+  //   {"param": "Vadi - Samvadi", "val": "Pancham - Shadj"},
+  //   {"param": "Time", "val": "2nd Prahar of the Night (9PM to 12AM)"},
+  //   {"param": "Vishranti Sthan", "val": "S P - S' P R;"},
+  //   {"param": "Mukhya Ang", "val": "g R ; m P ; m P D P ; D n S' ; n D P;"},
+  //   {"param": "Aaroh - Avroh", "val": "S R g m P D n S'; - S' n D P m g R S;"}
+  // ];
 
-  final response = await http.get(Uri.parse(url));
+  List<Map<String, String>> parsedTable = [];
+  List<String> parsedParas = [];
+
+  final response = await http.get(
+      Uri.parse(url.startsWith("https") ? url : "https://tanarang.com$url"));
 
   if (response.statusCode == 200) {
-    var ragaDetails =
-        parse(response.body).querySelector("tbody")!.querySelectorAll("tr");
+    var ragaDetails = parse(response.body);
 
-    for (var ragaDetail in ragaDetails) {
+    var ragaTable = ragaDetails.querySelector("tbody")!.querySelectorAll("tr");
+
+    for (var ragaDetail in ragaTable) {
       var ragaDetailList = ragaDetail.querySelectorAll("td");
-      parsedData.add({
+      parsedTable.add({
         "param": ragaDetailList[0].querySelector("a")!.text,
         "val": ragaDetailList[1].text
       });
     }
 
-    return parsedData;
+    var h4 = ragaDetails.querySelector("h4");
+    var paraIndex = h4!.parent!.children.indexOf(h4) - 1;
+
+    parsedParas = ragaDetails
+        .querySelectorAll("p")
+        .sublist(0, paraIndex)
+        .map((p) => p.text)
+        .toList();
+
+    return (parsedTable, parsedParas);
   } else {
     throw Exception('Failed to load raga data');
   }
@@ -54,7 +83,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DailyRaga',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'DailyRaga'),
@@ -103,39 +132,57 @@ class _MyHomePageState extends State<MyHomePage> {
                   if (snapshot.hasError) {
                     return Text('Error: ${snapshot.error}');
                   } else {
-                    return Column(
-                      children: [
-                        Text(snapshot.data!["name"]!),
-                        FutureBuilder(
-                          future: fetchRagaDetails(snapshot.data!["url"]!),
-                          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-                            switch (snapshot.connectionState) {
-                              case ConnectionState.waiting:
-                                return const Text('Loading....');
-                              default:
-                                if (snapshot.hasError) {
-                                  return Text('Error: ${snapshot.error}');
-                                } else {
-                                  return DataTable(
-                                    columns: const [
-                                      DataColumn(label: Text('Parameter')),
-                                      DataColumn(label: Text('Value')),
-                                    ],
-                                    rows: snapshot.data.map<DataRow>((row) {
-                                      return DataRow(cells: [
-                                        DataCell(Text(row['param'] ?? '')),
-                                        DataCell(Text(row['val'] ?? '')),
-                                      ]);
-                                    }).toList(),
-                                  );
-                                }
-                            }
-                          },
-                        ),
-                        ElevatedButton(
-                            onPressed: () => fetchRaga(),
-                            child: const Text('Fetch Random Raga'))
-                      ],
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Text("Raga ${snapshot.data!["name"]!}"),
+                          FutureBuilder(
+                            future: fetchRagaDetails(snapshot.data!["url"]!),
+                            builder:
+                                (BuildContext ctx, AsyncSnapshot snapshot) {
+                              switch (snapshot.connectionState) {
+                                case ConnectionState.waiting:
+                                  return const Text('Loading....');
+                                default:
+                                  if (snapshot.hasError) {
+                                    return Text('Error: ${snapshot.error}');
+                                  } else {
+                                    var (ragaTable, ragaParas) = snapshot.data;
+
+                                    return Column(
+                                      children: <Widget>[
+                                            DataTable(
+                                              columns: const [
+                                                DataColumn(
+                                                    label: Text('Parameter')),
+                                                DataColumn(
+                                                    label: Text('Value')),
+                                              ],
+                                              rows:
+                                                  ragaTable.map<DataRow>((row) {
+                                                return DataRow(cells: [
+                                                  DataCell(
+                                                      Text(row['param'] ?? '')),
+                                                  DataCell(
+                                                      Text(row['val'] ?? '')),
+                                                ]);
+                                              }).toList(),
+                                            ),
+                                          ] +
+                                          <Text>[
+                                            for (var para in ragaParas)
+                                              Text(para)
+                                          ],
+                                    );
+                                  }
+                              }
+                            },
+                          ),
+                          ElevatedButton(
+                              onPressed: () => fetchRaga(),
+                              child: const Text('Fetch Random Raga'))
+                        ],
+                      ),
                     );
                   }
               }
