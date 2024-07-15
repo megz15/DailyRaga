b419e141aa9b5a83971282258019dd20cbca165a
megz15 <meghraj.g16@gmail.com>
2024-07-15 23:50:19 +0530

    feat: show all data

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
 
 Future<Map<String, String>> fetchRandomRaga() async {
-  return {"name": "Bhoopali", "url": "/wordpress/raag-bhoopali/"};
+  // return {
+  //   "name": "Bhoopali",
+  //   "url": "/wordpress/raag-bhoopali/",
+  // };
 
   final response =
       await http.get(Uri.parse("https://tanarang.com/raag-index/"));
@@ -23,29 +26,29 @@ Future<Map<String, String>> fetchRandomRaga() async {
 
 Future<(List<Map<String, String>>, List<String>)> fetchRagaDetails(
     String url) async {
-  return (
-    [
-      {
-        "param": "Swar",
-        "val": "Gandhar and Nishad Komal. Rest all Shuddha Swaras."
-      },
-      {"param": "Jati", "val": "Sampurna - Sampurna"},
-      {"param": "Thaat", "val": "Kafi"},
-      {"param": "Vadi - Samvadi", "val": "Pancham - Shadj"},
-      {"param": "Time", "val": "2nd Prahar of the Night (9PM to 12AM)"},
-      {"param": "Vishranti Sthan", "val": "S P - S' P R;"},
-      {"param": "Mukhya Ang", "val": "g R ; m P ; m P D P ; D n S' ; n D P;"},
-      {"param": "Aaroh - Avroh", "val": "S R g m P D n S'; - S' n D P m g R S;"}
-    ],
-    [
-      "",
-      "Raag Bhoopali is a tranquil soft melody that fills up a new life force in the environment with the dominant Gandhar strengthened by the Swayambhu Gandhar from the Kharja of the Taanpura. It belongs to Kalyan Thaat and is also known as Raag Bhoop.",
-      "In Raag Bhoopali, one should be very careful not to use Dhaivat as a resting note and not give much prominence to Dhaivat, otherwise Raag Deshkar will come into existence which has the same set of Notes as Raag Bhoopali. While taking a Meend from Shadj to Dhaivat or from Pancham to Gandhar, one should be very careful about not using Nishad and Madhyam respectively as a Kan-Swar, otherwise Raag Shuddha Kalyan will come into existence. Gandhar-Dhaivat Sangati is very important in Bhoopali and Rishabh is the Nyas Swar. ",
-      "Raag Bhoopali is popular as Raag Mohan in Carnatic Music. It is a Poorvang Pradhan Raag and is mostly sung in Madhya and Mandra octaves. It creates a deep and soothing atmosphere. The following illustrative combinations clearly bring out the melodic format:",
-      "S ; S ,D S R G ; R G S R ,D S ; S R G P ; P G R G ; R P G ; G S R ; R ,D S ; G R G ; P G ; P D P P ; D P ; G P R G R S ,D S ; S R G R G P D S’ ; P D P S’ ; S’ S’ ; R’ S’ D S’ ; D S’ R’ G’ R’ S’ ; D S’ D P G R G ; P R G R S ; R ,D S ; ",
-      ""
-    ]
-  );
+  // return (
+  //   [
+  //     {
+  //       "param": "Swar",
+  //       "val": "Gandhar and Nishad Komal. Rest all Shuddha Swaras."
+  //     },
+  //     {"param": "Jati", "val": "Sampurna - Sampurna"},
+  //     {"param": "Thaat", "val": "Kafi"},
+  //     {"param": "Vadi - Samvadi", "val": "Pancham - Shadj"},
+  //     {"param": "Time", "val": "2nd Prahar of the Night (9PM to 12AM)"},
+  //     {"param": "Vishranti Sthan", "val": "S P - S' P R;"},
+  //     {"param": "Mukhya Ang", "val": "g R ; m P ; m P D P ; D n S' ; n D P;"},
+  //     {"param": "Aaroh - Avroh", "val": "S R g m P D n S'; - S' n D P m g R S;"}
+  //   ],
+  //   [
+  //     "",
+  //     "Raag Bhoopali is a tranquil soft melody that fills up a new life force in the environment with the dominant Gandhar strengthened by the Swayambhu Gandhar from the Kharja of the Taanpura. It belongs to Kalyan Thaat and is also known as Raag Bhoop.",
+  //     "In Raag Bhoopali, one should be very careful not to use Dhaivat as a resting note and not give much prominence to Dhaivat, otherwise Raag Deshkar will come into existence which has the same set of Notes as Raag Bhoopali. While taking a Meend from Shadj to Dhaivat or from Pancham to Gandhar, one should be very careful about not using Nishad and Madhyam respectively as a Kan-Swar, otherwise Raag Shuddha Kalyan will come into existence. Gandhar-Dhaivat Sangati is very important in Bhoopali and Rishabh is the Nyas Swar. ",
+  //     "Raag Bhoopali is popular as Raag Mohan in Carnatic Music. It is a Poorvang Pradhan Raag and is mostly sung in Madhya and Mandra octaves. It creates a deep and soothing atmosphere. The following illustrative combinations clearly bring out the melodic format:",
+  //     "S ; S ,D S R G ; R G S R ,D S ; S R G P ; P G R G ; R P G ; G S R ; R ,D S ; G R G ; P G ; P D P P ; D P ; G P R G R S ,D S ; S R G R G P D S’ ; P D P S’ ; S’ S’ ; R’ S’ D S’ ; D S’ R’ G’ R’ S’ ; D S’ D P G R G ; P R G R S ; R ,D S ; ",
+  //     ""
+  //   ]
+  // );
 
   List<Map<String, String>> parsedTable = [];
   List<String> parsedParas = [];
@@ -74,6 +77,8 @@ Future<(List<Map<String, String>>, List<String>)> fetchRagaDetails(
         .sublist(0, paraIndex)
         .map((p) => p.text)
         .toList();
+
+    parsedParas.removeWhere((item) => item.isEmpty);
 
     return (parsedTable, parsedParas);
   } else {
@@ -137,61 +142,80 @@ class _MyHomePageState extends State<MyHomePage> {
                 AsyncSnapshot<Map<String, String>> snapshot) {
               switch (snapshot.connectionState) {
                 case ConnectionState.waiting:
-                  return const Text('Loading....');
+                  return const Center(child: CircularProgressIndicator());
                 default:
                   if (snapshot.hasError) {
                     return Text('Error: ${snapshot.error}');
                   } else {
-                    return SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Text("Raga ${snapshot.data!["name"]!}"),
-                          FutureBuilder(
-                            future: fetchRagaDetails(snapshot.data!["url"]!),
-                            builder:
-                                (BuildContext ctx, AsyncSnapshot snapshot) {
-                              switch (snapshot.connectionState) {
-                                case ConnectionState.waiting:
-                                  return const Text('Loading....');
-                                default:
-                                  if (snapshot.hasError) {
-                                    return Text('Error: ${snapshot.error}');
-                                  } else {
-                                    var (ragaTable, ragaParas) = snapshot.data;
+                    return Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Wrap(
+                              alignment: WrapAlignment.center,
+                              children: [
+                                Text(
+                                  "Raga ${snapshot.data!["name"]!}",
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(width: 16),
+                                ElevatedButton(
+                                  onPressed: () => fetchRaga(),
+                                  child: const Text('Fetch New Raga'),
+                                )
+                              ],
+                            ),
+                            FutureBuilder(
+                              future: fetchRagaDetails(snapshot.data!["url"]!),
+                              builder:
+                                  (BuildContext ctx, AsyncSnapshot snapshot) {
+                                switch (snapshot.connectionState) {
+                                  case ConnectionState.waiting:
+                                    return const Center(
+                                        child: CircularProgressIndicator());
+                                  default:
+                                    if (snapshot.hasError) {
+                                      return Text('Error: ${snapshot.error}');
+                                    } else {
+                                      var (ragaTable, ragaParas) =
+                                          snapshot.data;
 
-                                    return Column(
-                                      children: <Widget>[
-                                            DataTable(
-                                              columns: const [
-                                                DataColumn(
-                                                    label: Text('Parameter')),
-                                                DataColumn(
-                                                    label: Text('Value')),
-                                              ],
-                                              rows:
-                                                  ragaTable.map<DataRow>((row) {
-                                                return DataRow(cells: [
-                                                  DataCell(
-                                                      Text(row['param'] ?? '')),
-                                                  DataCell(
-                                                      Text(row['val'] ?? '')),
-                                                ]);
-                                              }).toList(),
-                                            ),
-                                          ] +
-                                          <Text>[
-                                            for (var para in ragaParas)
-                                              Text(para)
-                                          ],
-                                    );
-                                  }
-                              }
-                            },
-                          ),
-                          ElevatedButton(
-                              onPressed: () => fetchRaga(),
-                              child: const Text('Fetch Random Raga'))
-                        ],
+                                      return Column(children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              16, 0, 16, 0),
+                                          child: ListView(
+                                            shrinkWrap: true,
+                                            children: ragaTable
+                                                .map<Widget>((row) => ListTile(
+                                                      title: Text(
+                                                          row['param'] ?? '',
+                                                          style: const TextStyle(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w500)),
+                                                      subtitle: Text(
+                                                          row['val'] ?? ''),
+                                                    ))
+                                                .toList(),
+                                          ),
+                                        ),
+                                        ...ragaParas.map((para) => Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 16),
+                                              child: Text(para),
+                                            ))
+                                      ]);
+                                    }
+                                }
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   }
